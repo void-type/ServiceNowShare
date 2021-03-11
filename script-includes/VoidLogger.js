e7430276df1af80b916de82abe474cb3b229ccfd
Jeff Schreiner <Schreinerj@prpa.org>
2021-03-11 13:37:38 -0700

    Minimum level overrides

--- script-includes/VoidLogger.js
+++ script-includes/VoidLogger.js
@@ -5,43 +5,48 @@ var VoidLogger = Class.create();
 VoidLogger.prototype = {
   initialize: function (contextName, minimumLevel) {
     this._contextName = contextName;
-    this._minimumLogLevel = this._getLevel(minimumLevel) || this._getLevel('debug');
+
+    var minimumLevelOverride = gs.getProperty('logger.minimumLevel.' + contextName);
+
+    this._minimumLogLevel = this._getLevel(minimumLevelOverride) ||
+      this._getLevel(minimumLevel) ||
+      this._getLevel('debug');
   },
 
   _contextName: null,
   _minimumLogLevel: null,
 
-  _logLevels: [{
-    name: 'error',
-    value: 4,
-    logger: gs.error,
+  _getLogLevels: function () {
+    return [
+      {
+        name: 'error',
+        value: 4,
+        logger: gs.error,
+      },
+      {
+        name: 'warn',
+        value: 3,
+        logger: gs.warn,
+      },
+      {
+        name: 'info',
+        value: 2,
+        logger: gs.info,
+      },
+      {
+        name: 'debug',
+        value: 1,
+        logger: gs.debug,
+      },
+    ];
   },
-  {
-    name: 'warn',
-    value: 3,
-    logger: gs.warn,
-  },
-  {
-    name: 'info',
-    value: 2,
-    logger: gs.info,
-  },
-  {
-    name: 'debug',
-    value: 1,
-    logger: gs.debug,
-  },
-  ],
 
   _getLevel: function (value) {
-    return this._logLevels.filter(function (level) {
+    return this._getLogLevels().filter(function (level) {
       return level.name === value || level.value === value;
     })[0];
   },
 
-  // Log a message, object, and exception using a certain logging level.
-  // All params are optional and can be replaced with null.
-  // If the logger's minimum level is higher than the logged level, the entry will not be logged.
   log: function (level, message, variableObject, exception) {
     var logLevel = this._getLevel(level) || this._getLevel('error');
 
@@ -69,19 +74,21 @@ VoidLogger.prototype = {
 };
 
 // Set your context name and minimum log level here. I recommend 'warn' for production.
-var logger = new VoidLogger('My Script', 'info');
+// When not in development, you can override the minimum level with a system property called "logger.minimumLevel.My Script" and value of the desired minimum level.
+var logger = new VoidLogger('My Script', 'warn');
 
 // Collect any other data you want to log in an object. This data can help you diagnose issues.
-var variables = {var1: 'something that should be known', var2: 333};
+var variables = { var1: 'something that should be known', var2: 333 };
 
 // Example usages.
-logger.log('info', 'We got here');
-logger.log('info', 'We got here', variables);
+logger.log('info', 'This won\'t be logged');
+logger.log('warn', 'We got here');
+logger.log('warn', 'We got here', variables);
 
 try {
   throw 'something';
-} catch(ex) {
+} catch (ex) {
 
   // Example usage with exception.
-    logger.log('error', 'something happened', variables, ex);
+  logger.log('error', 'something happened', variables, ex);
 }
