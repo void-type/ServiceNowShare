2abddf1598ac7530631de78e190c1b39b23f4be7
Jeff Schreiner <Schreinerj@prpa.org>
2021-03-15 12:11:42 -0600

    Add logger readme

--- script-includes/VoidLogger.js
+++ script-includes/VoidLogger.js
@@ -87,25 +87,3 @@ VoidLogger.prototype = {
 
   type: 'VoidLogger'
 };
-
-// Build a logger with a context name. You can optionally pass a minimum log level; the default minimum level is 'warn.'
-// You can override the minimum level with a system property called "logger.minimumLevel.My Script" and value of the desired minimum level.
-// To stop overriding, you need to clear the property value before you delete property. This is because SN caches these properties.
-var logger = new VoidLogger('My Script');
-
-// Example usages.
-logger.info('This won\'t be logged because of our minimum logging level.');
-logger.warn('We got here.');
-
-// Collect any other data you want to log in an object. This data can help you diagnose issues.
-var variables = { var1: 'something that should be known', var2: 333 };
-logger.warn('Add an object to be stringified.', variables);
-
-logger.log('warn', 'We can set the log level as a string using "log"');
-
-try {
-  throw 'something';
-} catch (ex) {
-
-  logger.error('Use the exception parameter!', variables, ex);
-}
